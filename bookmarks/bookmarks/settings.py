2879d4a336694cb72bcd5596613c969b6c7086f9
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-23 17:50:34 +0300

    Add resetting password

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -20,6 +20,12 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Application definition
 
 INSTALLED_APPS = [
