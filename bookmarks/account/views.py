e0ad6589d1b28e0a73aaba5efe91e89d0ccd1db4
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-23 14:05:30 +0300

    Add auth

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 
@@ -22,3 +23,7 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -16,6 +16,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
 # Application definition
 
