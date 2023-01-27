6a79bcf61d7b3a077a4a4a83f996df426a180b10
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-27 21:05:42 +0300

    Add scrolling pagination for images

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required
 def image_create(request):
@@ -49,3 +50,31 @@ def image_like(request):
             pass
 
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
