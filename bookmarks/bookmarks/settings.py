b41e63e7d9d451d09731bd00828abf526a0e47e6
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-29 12:06:39 +0300

    Add a follow system

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,3 +163,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
