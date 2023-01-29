7acc10c3ff9f7df1780f9e1e5e7281f1c6a7c50d
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-29 14:57:05 +0300

    Add activity stream

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -8,6 +8,8 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from actions.utils import create_action
+from actions.models import Action
 
 
 # def user_login(request):
@@ -31,7 +33,15 @@ from django.views.decorators.http import require_POST
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
 
 
 def register(request):
@@ -42,6 +52,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
 
     else:
@@ -92,6 +103,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
 
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -6,6 +6,7 @@ from .models import Image
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 @login_required
 def image_create(request):
@@ -16,6 +17,7 @@ def image_create(request):
             new_image = form.save(commit=False)
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(request, 'Image added successfully')
             return redirect(new_image.get_absolute_url())
 
@@ -40,6 +42,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
 
             else:
                 image.users_like.remove(request.user)
